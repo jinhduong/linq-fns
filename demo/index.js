bc460944083b431a27b6bf8680a0a6c4e1857c68
jinhduong <0961020@gmail.com>
2018-04-23 23:42:10 +0800

    Update docs

--- demo/index.js
+++ demo/index.js
@@ -3,39 +3,56 @@ import { Queryable } from '../dist/out-tsc/index.js';
 const queryable = new Queryable();
 
 let promiseApi = new Promise((resolve, reject) => {
-    // Fake data from api
+    // skills: attack, stamia, speed, shoot
     setTimeout(() => {
         console.log('...get data');
         resolve([
-            { name: 'Ronaldo', overall: 96, nationId: 1, skills: [97, 90, 86, 95] },
-            { name: 'Messi', overall: 98, nationId: 2, skills: [97, 90, 86, 95] },
-            { name: 'Mbappe', overall: 86, nationId: 3, skills: [97, 90, 86, 95] },
-            { name: 'Salah', overall: 89, nationId: 4, skills: [97, 90, 86, 95] }
+            { name: 'Ronaldo', overall: 96, nationId: 1, skills: [96, 85, 87, 91] },
+            { name: 'Messi', overall: 98, nationId: 2, skills: [97, 85, 91, 93] },
+            { name: 'Mbappe', overall: 86, nationId: 3, skills: [89, 81, 95, 83] },
+            { name: 'Salah', overall: 89, nationId: 4, skills: [88, 82, 97, 86] }
         ]);
     }, 1000);
 })
 
-let staticLoopkup = [
+let nations = [
     { id: 1, name: 'Portugal', areaId: 1 },
     { id: 2, name: 'Argentina', areaId: 2 },
     { id: 3, name: 'France', areaId: 1 },
     { id: 4, name: 'Egypt', areaId: 3 }
 ]
 
-let staticAreas = [
+let continents = [
     { id: 1, areaName: 'Euro' },
     { id: 2, areaName: 'South America' },
 ]
 
-async function main() {
+function main() {
     // Just query not execute query
+    console.time('querytime');
     let query = queryable
         .from(promiseApi)
-        .where(x => x.overall > 90)
-        .selectMany(x => x.skills);
+        .join(nations, (x, y) => x.nationId === y.id)
+        .leftJoin(continents, (o, z) => o.y.areaId === z.id)
+        .select(o => {
+            return {
+                playerName: o.x.name,
+                overall: o.x.overall,
+                nation: o.y.name,
+                area: o.areaName,
+            }
+        })
+        .groupBy(x => x.area)
+        .orderByDescending(x => x.key);
 
-    const data = await query.toList();
-    console.log(data);
+    console.timeEnd('querytime');
+
+    console.time('executetime');
+    const data = query.toList().then(data => {
+        console.timeEnd('executetime');
+        console.log(data);
+        console.table(data);
+    });
 }
 
 main();
